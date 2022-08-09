f3ba6dbbceae9f748ebee0434f8d1919e6260e19
vanessabonis <vanessa1302@gmail.com>
2022-08-08 23:30:36 -0300

    iniciando spring security, falta jwt

---
+++ gerenciador-de-tarefas-back/gerenciador-de-tarefas/src/main/java/com/vanessabonis/gerenciadordetarefas/WebSecurityConfig.java
@@ -0,0 +1,49 @@
+package com.vanessabonis.gerenciadordetarefas;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+@Configuration
+//@EnableWebSecurity
+public class WebSecurityConfig {
+
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .httpBasic()
+//                .and()
+//                .authorizeHttpRequests()
+//                .anyRequest().authenticated()
+//                .and()
+//                .csrf().disable();
+//        return http.build();
+//    }
+
+    @Bean
+    public FilterRegistrationBean processCorsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Arrays.asList("http://localhost:8080", "http://localhost:8081"));
+
+		config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        final FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
+}
